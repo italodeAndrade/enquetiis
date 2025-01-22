51682a408f1017e55233bf54d5cc69d342d40245
Ítalo de Andrade <italolande@gmail.com>
2025-01-22 10:53:19 -0300

    home
    Neste commit, implementei a visibilidade de todas as enquetes que estão sendo armazenadas no banco de dados para a página HTML, utilizando o Node.js como backend.

--- main/server.js
+++ main/server.js
@@ -10,7 +10,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
-
 app.listen(PORT, () => {
     console.log(`rodando: http://localhost:${PORT}`);
+});
+
+app.get('/load_enquetes', (req, res) => {
+    const query = 'SELECT id, titulo, descricao, data_init,data_term FROM enquete';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Erro na consulta de dados dados:', err);
+            res.status(500).send('Erro ao buscar dados');
+        } else {
+            res.json(results); 
+        }
+    });
 });
